d671d5b41be7432d61bd3ed0bbbdc44ae58723db
BehrangBina <BehrangBina@hotmail.com>
2016-09-27 13:17:44 +1000

    bootstrap and first migeration

---
+++ GeekEvent/Models/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace GeekEvent.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        //add the model
+        public DbSet<Gig> Gigs { get; set; }
+        //2 - we wanna query genre so we add it
+        public DbSet<Genre> Genres { get; set; }
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+            
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
