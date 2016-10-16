77039196cf99fe161770153756b075a89ed32845
BehrangBina <BehrangBina@hotmail.com>
2016-10-16 15:36:00 +1100

    Gig modelview added

---
+++ GeekEvent/ViewModel/GigFormViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GeekEvent.ViewModel
+{
+    public class GigFormViewModel
+    {
+        //Helper Cless combines seperated date time to one field
+        public string Venu { get; set; }
+        public string Date  { get; set; }
+        public string Time { get; set; }
+    }
+}
