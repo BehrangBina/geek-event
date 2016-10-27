3cee7f18d62e9d0a6816146d2f91691fa1547dc4
BehrangBina <BehrangBina@hotmail.com>
2016-10-27 23:05:52 +1100

    going gigs front-end added

--- GeekEvent/Controllers/HomeController.cs
+++ GeekEvent/Controllers/HomeController.cs
@@ -1,16 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GeekEvent.Models;
 
 namespace GeekEvent.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public HomeController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var upcomingGigs = _context.Gigs
+                .Include(g => g.Artist)
+                .Include(g=>g.Genre)
+                .Where(g=>g.DateTime > DateTime.Now);
+            return View(upcomingGigs);
         }
 
         public ActionResult About()
