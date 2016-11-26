63ed17b8b35851ccb155cf72133b426c68040892
BehrangBina <BehrangBina@hotmail.com>
2016-11-26 19:31:44 +1100

    API controller for gigs

---
+++ GeekEvent/Controllers/AttandanceController.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Http;
+using GeekEvent.Models;
+using Microsoft.AspNet.Identity;
+
+namespace GeekEvent.Controllers
+{
+    [Authorize]
+    public class AttandanceController : ApiController
+    {
+        public ApplicationDbContext _Context;
+
+        public AttandanceController()
+        {
+            _Context=new ApplicationDbContext();
+        }
+      
+        public  IHttpActionResult Attend(int gigId)
+        {
+            var attendance = new Attendance
+            {
+                GigId = gigId,
+                AttendeeId = User.Identity.GetUserName()
+            };
+            _Context.Attendances.Add(attendance);
+            _Context.SaveChanges();
+            return Ok();
+        }
+    }
+}
